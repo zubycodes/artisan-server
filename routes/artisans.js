2b866fcb96650f50b4956cf84159f54d8a5e26c2
hisduteamb <belalmughal@gmail.com>
2025-03-19 01:31:25 +0500

    Z

--- routes/artisans.js
+++ routes/artisans.js
@@ -347,7 +347,7 @@ module.exports = (dependencies) => {
     // Create a new artisan with related data
     create: [
       upload.fields([
-        { name: 'profile_picture', maxCount: 1 },
+       /*  { name: 'profile_picture', maxCount: 1 }, */
         /*    { name: 'product_images', maxCount: 5 } // Adjust maxCount as needed */
       ]),
       validateArtisanData,
@@ -366,13 +366,13 @@ module.exports = (dependencies) => {
         }
         try {
           const { artisan, trainings, loans, machines } = req.body;
-          const profilePicturePath = req.files ? req.files['artisan[profile_picture]'] ? req.files['artisan[profile_picture]'].path : null : null;
-
+         /*  const profilePicturePath = req.files ? req.files['artisan[profile_picture]'] ? req.files['artisan[profile_picture]'].path : null : null;
+ */
           res.write(`data: ${JSON.stringify({ status: 'progress', message: 'Creating artisan...' })}\n\n`);
           routeLogger.info('Creating artisan...');
           routeLogger.info({ artisan, trainings, loans, machines }, 'artisan');
-          routeLogger.info({ profilePicturePath: profilePicturePath }, 'profilePicturePath');
-          const artisanId = await entityOps.createArtisan(artisan, profilePicturePath ?? artisan.profile_picture);
+          /* routeLogger.info({ profilePicturePath: profilePicturePath }, 'profilePicturePath'); */
+          const artisanId = await entityOps.createArtisan(artisan, '123');
           routeLogger.info({ artisanId }, 'Artisan created successfully');
 
           res.write(`data: ${JSON.stringify({ status: 'progress', message: 'Creating trainings...' })}\n\n`);
