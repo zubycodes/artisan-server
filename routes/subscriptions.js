416cd8c11fa9c75df11a985feb16e3e83e1f2911
hisduteamb <belalmughal@gmail.com>
2025-04-11 07:41:52 +0500

    Z

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { dbAsync, createHandler } = require("./base_route.js");
-import nodemailer from "nodemailer";
+const nodemailer = require("nodemailer");
 
 /**
  * Email subscription operations
